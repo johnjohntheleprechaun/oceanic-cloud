3887bf3de23399c5a8977134934522373d3b38e9
John Jorgensen <john@jorgensenfamily.us>
2024-01-28 23:17:05 -0800

    add config file

--- bin/oceanic-cloud.ts
+++ bin/oceanic-cloud.ts
@@ -2,7 +2,16 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { OceanicCloudStack } from '../lib/oceanic-cloud-stack';
+import {  readFileSync } from 'fs';
+
+const configFile = readFileSync("config.json");
+const config = JSON.parse(configFile.toString());
+console.log(config);
 
 const app = new cdk.App();
-new OceanicCloudStack(app, "OceanicProd");
-new OceanicCloudStack(app, "OceanicTest");
+const prodProps = config.prodProps;
+prodProps.isProd = true;
+new OceanicCloudStack(app, "OceanicProd", prodProps);
+const testProps = config.testProps;
+testProps.isProd = false;
+new OceanicCloudStack(app, "OceanicTest", testProps);
