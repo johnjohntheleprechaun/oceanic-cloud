9ffc7d5c651c3759e9896839bf0ce8fa2c6b1702
John Jorgensen <john@jorgensenfamily.us>
2024-03-07 11:26:48 -0800

    remove unused ts files

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-api_endpoint = "https://j7d7wuwdb7.execute-api.us-west-2.amazonaws.com/prod"
-oceanic_endpoint = "https://oceanic.api.leprechaun.dev"
-local_endpoint = "http://localhost:3000"
-req = requests.post(local_endpoint + "/signup", json={"username": "test", "password": "password", "publicKey": "", "privateKey": ""})
-print(req.text)
