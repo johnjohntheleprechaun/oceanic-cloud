03f91e781c06ddeac762c8483a26a816be02699e
John Jorgensen <john@jorgensenfamily.us>
2024-03-07 11:29:52 -0800

    update test endpoint

--- lib/functions/test.ts
+++ lib/functions/test.ts
@@ -1,12 +1,11 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyHandler, APIGatewayProxyResult } from "aws-lambda";
 
 export const handler: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
     return {
         statusCode: 200,
         body: JSON.stringify({
             tableName: process.env.DYNAMO_TABLE,
-            s3Bucket: process.env.BUCKET
+            bucketName: process.env.BUCKET
         })
-    }
+    };
 };
