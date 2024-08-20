db0c54e5659818b96c9abf25c55f32561ee7a5d5
John Jorgensen <john@jorgensenfamily.us>
2024-08-20 14:56:10 +0000

    add schema compilation to init script

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 # this is required for compiling ARM lambdas on x86 machines
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# compile schemas
+npx ts-node schema-compiler.ts
