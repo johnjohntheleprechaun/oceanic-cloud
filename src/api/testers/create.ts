8f6ce05a29a05e4a2ae9ff4890795b2588e6c344
John Jorgensen <john@jorgensenfamily.us>
2024-08-29 10:59:55 -0600

    rewrite document creation tester

--- src/api/testers/create.ts
+++ src/api/testers/create.ts
@@ -1,24 +1,26 @@
 import {DocumentCreate} from "../schema-types/document-create"
 
-const uuid = crypto.randomUUID();
-const request: DocumentCreate = {
-    type: "messages-journal",
-    documentKey: "",
-    id: uuid,
-};
-const url = process.argv[process.argv.length - 2] + "/users/me/documents";
-const token = process.argv[process.argv.length - 1];
-console.log(token);
-fetch(url, {
-    method: "POST",
-    body: JSON.stringify(request),
-    headers: {
-        "Authorization": token,
-    },
-})
-    .then(resp => resp.json())
-    .then(json => console.log(json, "JSON RESP"))
-    .then(() => console.log("got the thing"))
-    .then(() => fetch(`${url}/${uuid}`, {headers: {"Authorization": token}}))
-    .then(resp => resp.json())
-    .then(json => console.log(json))
+async function documentCreateTest() {
+    const uuid = crypto.randomUUID();
+    const request: DocumentCreate = {
+        type: "messages-journal",
+        documentKey: "",
+        id: uuid,
+    };
+    const url = process.argv[process.argv.length - 2] + "/users/me/documents";
+    const token = process.argv[process.argv.length - 1];
+    console.log(token);
+    const newDocument = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(request),
+        headers: {
+            "Authorization": token,
+        },
+    }).then(resp => resp.json())
+    console.log(newDocument);
+
+    const fetchedDocument = await fetch(`${url}/${uuid}`, {headers: {"Authorization": token}}).then(resp => resp.json());
+    console.log(fetchedDocument);
+}
+
+documentCreateTest();
