3da3301e6c231fb37e16236e4a53e8a686f606ce
John Jorgensen <john@leprechaun.dev>
2024-11-12 20:11:07 -0700

    dynamo utils go brrr

---
+++ src/api/utils/dynamo.ts
@@ -0,0 +1,51 @@
+import {DynamoDBClient, GetItemCommand, GetItemCommandOutput, PutItemCommand} from "@aws-sdk/client-dynamodb";
+import {unmarshall} from "@aws-sdk/util-dynamodb";
+import addFormats from "ajv-formats";
+import Ajv from "ajv";
+
+const ajv = new Ajv();
+addFormats(ajv);
+ajv.addKeyword({
+    keyword: "isBuffer",
+    type: "object",
+    schema: false,
+    validate: (data: any) => Buffer.isBuffer(data),
+    errors: false,
+});
+
+export async function getItemWithSchema(command: GetItemCommand, schema: object, client?: DynamoDBClient): Promise<object> {
+    if (!client) {
+        client = new DynamoDBClient();
+    }
+    const resp = await client.send(command);
+    if (!resp.Item) {
+        // undefined error
+        return {};
+    }
+    const item = unmarshall(resp.Item);
+
+    if (ajv.validate(schema, item)) {
+        return item;
+    }
+    else {
+        throw new Error();
+    }
+}
+
+export async function putItemWithSchema(command: PutItemCommand, schema: object, client?: DynamoDBClient): Promise<GetItemCommandOutput> {
+    if (!client) {
+        client = new DynamoDBClient();
+    }
+    if (!command.input.Item) {
+        // error
+        throw new Error();
+    }
+    const item = unmarshall(command.input.Item);
+
+    if (ajv.validate(schema, item)) {
+        return client.send(command);
+    }
+    else {
+        throw new Error();
+    }
+}
