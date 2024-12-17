0c25040acd12399e10fbd70caab862b6e583b646
John Jorgensen <john@leprechaun.dev>
2024-12-17 14:51:23 -0700

    update doc create tester

--- src/api/testers/create.ts
+++ src/api/testers/create.ts
@@ -4,22 +4,30 @@ async function documentCreateTest() {
     const uuid = crypto.randomUUID();
     const request: DocumentCreateRequest = {
         type: "messages-journal",
-        documentKey: "",
+        documentKey: "00",
         id: uuid,
     };
+    console.log(request);
     const url = process.argv[process.argv.length - 2] + "/users/me/documents";
     const token = process.argv[process.argv.length - 1];
     console.log(token);
+    console.log(url)
     const newDocument = await fetch(url, {
         method: "POST",
         body: JSON.stringify(request),
         headers: {
             "Authorization": token,
         },
-    }).then(resp => resp.json())
+    }).then(async resp => [await resp.json(), resp.status])
     console.log(newDocument);
 
-    const fetchedDocument = await fetch(`${url}/${uuid}`, {headers: {"Authorization": token}}).then(resp => resp.json());
+    let documentUrl = `${url}/${uuid}`
+    console.log(documentUrl)
+    const fetchedDocument = await fetch(documentUrl, {
+        headers: {
+            'Authorization': token
+        }
+    }).then(resp => resp.json());
     console.log(fetchedDocument);
 }
 
